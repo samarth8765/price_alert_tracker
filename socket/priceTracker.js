af73b82e86fcb976d453752bc452b9469bf92bbf
vizz <samdhawan8765@gmail.com>
2024-02-04 03:59:50 +0530

    Implement websocket stream

---
+++ socket/priceTracker.js
@@ -0,0 +1,69 @@
+import WebSocket from 'ws';
+import { Alert } from '../models/alert.js'
+
+const alertsSubscriptions = new Map();
+
+function subscribeToCurrencyPair(pair) {
+    if (alertsSubscriptions.has(pair)) return;
+
+    const ws = new WebSocket(`wss://stream.binance.com:9443/ws/${pair.toLowerCase()}@trade`);
+    alertsSubscriptions.set(pair, ws);
+
+    ws.on('message', (data) => {
+        const { s: symbol, p: price } = JSON.parse(data);
+        checkAndNotifyAlerts(symbol, parseFloat(price));
+    });
+
+    ws.on('close', () => {
+        alertsSubscriptions.delete(pair);
+        console.log(`${pair} WebSocket closed`);
+    });
+
+    console.log(`Subscribed to ${pair}`);
+}
+
+function closeAllConnections() {
+    alertsSubscriptions.forEach((ws, pair) => {
+        console.log(`Closing connection for ${pair}`);
+        ws.close();
+        alertsSubscriptions.delete(pair);
+    });
+}
+
+async function refreshSubscriptions() {
+    closeAllConnections();
+    const activeAlerts = await Alert.findAll({
+        where: { status: 'active' },
+        attributes: ['currencyPair']
+    });
+
+    const uniquePairs = new Set(activeAlerts.map(alert => alert.currencyPair));
+
+    uniquePairs.forEach(currencyPair => {
+        subscribeToCurrencyPair(currencyPair);
+    });
+}
+
+async function checkAndNotifyAlerts(symbol, currentPrice) {
+    const alerts = await Alert.findAll({ where: { currencyPair: symbol, status: 'active' } });
+
+    alerts.forEach(async (alert) => {
+        let isTriggered = false;
+        console.log(currentPrice, alert.targetPrice);
+
+        if ((alert.direction === 'above' && currentPrice >= alert.targetPrice) ||
+            (alert.direction === 'below' && currentPrice <= alert.targetPrice)) {
+            isTriggered = true;
+        }
+
+        if (isTriggered) {
+            alert.status = 'triggered';
+            await alert.save();
+            console.log("Price has changed", symbol, alert.id);
+            // sendEmailNotification(alert);
+        }
+    });
+}
+
+refreshSubscriptions();
+setInterval(refreshSubscriptions, 120000); 
